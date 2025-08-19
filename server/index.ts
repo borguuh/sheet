3e0f0cb341a88ad143a7f4dc8bc445556d5c6d2a
Borguuh <abdrahmanadewale@gmail.com>
2025-08-19 06:42:54 +0100

    dir fix

--- server/index.ts
+++ server/index.ts
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    const staticPath = path.join(__dirname, '../../dist/client');
+    const staticPath = path.join(__dirname, '../dist/client');
   serveStatic(app, staticPath)
   }
 
--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/client"),
+    outDir: path.resolve(__dirname, "../dist/client"), // Ensure this matches Vercel's expected path
     emptyOutDir: true,
   },
   server: {
